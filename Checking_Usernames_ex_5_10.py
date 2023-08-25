92c5ad1ac595f9309069a5dbd0e467dfc5a74f01
fazlarabbishanto <113310077+fazlarabbishanto@users.noreply.github.com>
2023-08-25 12:31:54 +0600

    Update Checking_Usernames_ex_5_10.py

--- Checking_Usernames_ex_5_10.py
+++ Checking_Usernames_ex_5_10.py
@@ -1,7 +1,7 @@
 current_users=['araf','akhtarul','mahraf','opu','shanto']
-new_users=['araf','akhtarul','jarin','arnob','mehefil']
+new_users=['araf','Akhtarul','Jarin','Arnob','mehefil']
 for username in new_users:
-    if username in current_users:
-        print(f"Hello {username.title()}.The username is already in use,please use a different one.")
+    if username.lower() in current_users :
+        print(f"Hello {username.title()} .The username is already in use,please use a different one.")
     else:
-        print("The username is avaiable.")
+        print(f"Hello {username.title()} .The username is avaiable.")
