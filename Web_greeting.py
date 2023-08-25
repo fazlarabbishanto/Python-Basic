c168d8ed533fcd442fbe65d4d2a8212602c2aab0
fazlarabbishanto <113310077+fazlarabbishanto@users.noreply.github.com>
2023-08-25 12:00:52 +0600

    Add files via upload

---
+++ Web_greeting.py
@@ -0,0 +1,6 @@
+﻿usernames=['Araf','Mehraf','Akhtarul','Opu','admin']
+for username in usernames:
+    if username=='admin':
+       print(f"Hello {username} , would you like to see a status report?")
+    else:
+        print(f"Hello {username},thank you for loging  again.")
