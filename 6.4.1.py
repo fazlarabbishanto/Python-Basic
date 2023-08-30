c84c547533b99b9c875e65705baff89cdf22a53c
fazlarabbishanto <113310077+fazlarabbishanto@users.noreply.github.com>
2023-08-31 03:18:52 +0600

    Add files via upload
    6-5. Rivers: Make a dictionary containing three major rivers and the country 
    each river runs through. One key-value pair might be 'nile': 'egypt'.
    •	 Use a loop to print a sentence about each river, such as The Nile runs 
    through Egypt.
    •	 Use a loop to print the name of each river included in the dictionary.
    •	 Use a loop to print the name of each country included in the dictionary.
    6-6. Polling: Use the code in favorite_languages.py (page 97).

---
+++ 6.4.1.py
@@ -0,0 +1,8 @@
+rivers={
+    'nile':'egypt',
+    'ganga':'india',
+    'jamuna':'bangladesh',
+    }
+for name,country in rivers.items():
+
+    print(f"The {name.title()} runs through {country.title()}")
