f10808ead37077d33815dd7219f47893ebdded5b
fazlarabbishanto <113310077+fazlarabbishanto@users.noreply.github.com>
2023-08-31 03:22:26 +0600

    Update 6.4.1.py

--- 6.4.1.py
+++ 6.4.1.py
@@ -6,3 +6,7 @@ rivers={
 for name,country in rivers.items():
 
     print(f"The {name.title()} runs through {country.title()}")
+    for river,country in rivers.items():
+    print(f"\nRiver:{river.title()}")
+    print(f"Country:{country.title()}")
+    
